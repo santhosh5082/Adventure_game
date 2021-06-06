1af1f32275ea5119ce26c1c7aab70ab93052ff6d
Santhosh Kumar <santhoshkumar00666@gmail.com>
2021-06-06 15:50:16 +0530

    edited from extra branch

--- game.py
+++ game.py
@@ -4,10 +4,10 @@ user_input = input("Would you like to play ? (Yes / No) ")
 
 if user_input.lower().strip() == 'yes' :
     
-    a = input("You reached a crossroad, would you like to go left or right ?").lower().strip()
+    a = input("You reached a crossroad, would you like to go left or right ? ").lower().strip()
     if a == 'left' :
         
-        b = input("You encountered Godzilla !, would you like to attack or run ?").lower().strip()
+        b = input("You encountered Godzilla !, would you like to attack or run ? ").lower().strip()
         if b == 'attack' :
             print('That was not a great idea, you lost !')
         else :yes
